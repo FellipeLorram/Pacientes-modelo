52c9e026ecad7b8bd661c539fed854b85a4a99f0
fellipe lorram <fellipelorram@gmail.com>
2021-08-17 08:50:56 -0300

    responsividade

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import NovoPaciente from "./NovoPaciente.js";
+
 const toggleSideBar = document.querySelector('.top_bar_toggle');
 const main = document.querySelector('.main');
 const navigation = document.querySelector('.navigation');
@@ -15,14 +17,17 @@ accordionItemHeaders.forEach(accordionItemHeader => {
 
         accordionItemHeader.classList.toggle("active");
         const accordionItemBody = accordionItemHeader.nextElementSibling;
+        const accordionFooter = accordionItemBody.querySelector('.box__paciente_body_footer');
         if (accordionItemHeader.classList.contains("active")) {
             accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
             accordionItemBody.parentElement.style.borderBottomRightRadius = 0;
             accordionItemBody.parentElement.style.marginBottom = "90px";
             accordionItemBody.parentElement.classList.add('active');
+            console.log(window.getComputedStyle(accordionItemBody.parentElement).getPropertyValue('width'))
+            if (window.screen.width <= 768) accordionFooter.style.width = window.getComputedStyle(accordionItemBody.parentElement).getPropertyValue('width')
             setTimeout(function () {
                 accordionItemBody.style.overflow = "visible"
-            }, 100)
+            }, 100);
         }
         else {
             accordionItemBody.parentElement.style.borderBottomRightRadius = "30px";
@@ -30,13 +35,30 @@ accordionItemHeaders.forEach(accordionItemHeader => {
             accordionItemBody.style.overflow = "hidden"
             accordionItemBody.style.maxHeight = 0;
             accordionItemBody.parentElement.classList.remove('active');
-            
         }
     });
 });
 
-// MOVER ADD PACIENTE
 
+// ADD NOVO PACIENTE
+const addNewPacienteMobile = document.querySelector('.add_new_paciente_content_mobile');
+
+document.querySelector('.add_new_paciente_content').addEventListener('click', e => {
+    if (!e.target.classList.contains('open_with') && !e.target.classList.contains('open_with_left_side')) NovoPaciente.open();
+});
+
+addNewPacienteMobile.addEventListener('click', e => {
+    if (!e.target.classList.contains('add_new_paciente_mobile_icon')) NovoPaciente.open();
+});
+
+
+addNewPacienteMobile.querySelector('.add_new_paciente_mobile_icon').addEventListener('click', e => {
+   e.target.classList.toggle('icon_rotate');
+    addNewPacienteMobile.classList.toggle('active');
+});
+
+
+// MOVER ADD PACIENTE
 const open_with = document.querySelector('.open_with');
 const open_with_left_side = document.querySelector('.open_with_left_side');
 
@@ -53,3 +75,31 @@ open_with_left_side.addEventListener('click', e => {
     e.target.style.display = "none";
     open_with.style.display = "block";
 });
+
+
+/*                  Esconder NavBar                      */
+let prevPageYOfsset = window.pageYOffset;
+window.onscroll = () => {
+    let currentPageYOfsset = window.pageYOffset;
+
+    if (prevPageYOfsset > currentPageYOfsset) {
+        document.querySelector(".main_top_bar").style.top = "0";
+        document.querySelector(".navigation_mobile").style.bottom = "0";
+        addNewPacienteMobile.classList.remove('desactive');
+    }
+    else {
+        document.querySelector(".main_top_bar").style.top = "-100px";
+        document.querySelector(".navigation_mobile").style.bottom = "-100px";
+        addNewPacienteMobile.classList.add('desactive');
+    }
+    prevPageYOfsset = currentPageYOfsset;
+
+    if (document.querySelector(".main_top_bar").style.top == "-100px") {
+        setTimeout(() => {
+            document.querySelector(".main_top_bar").style.top = "0";
+            document.querySelector(".navigation_mobile").style.bottom = "0";
+            addNewPacienteMobile.classList.remove('desactive');
+        }, 1563);
+
+    }
+}
