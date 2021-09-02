9ae7d3b37d0654abe8fe10c0f4a3a0a79d17af32
FellipeLorram <83411249+FellipeLorram@users.noreply.github.com>
2021-09-02 18:46:15 -0300

    Add files via upload

---
+++ Data.js
@@ -0,0 +1,111 @@
+export const Dias = [
+    {
+        dia: 'Segunda',
+        horario: {
+            '9:00': true,
+            '10:00': true,
+            '11:00': false,
+            '12:00': true,
+            '13:00': true,
+            '14:00': true,
+            '15:00': false,
+            '16:00': true,
+            '17:00': true,
+            '18:00': true,
+            '19:00': true,
+            '20:00': true,
+            '21:00': true,
+        }
+    },
+    {
+        dia: 'Terça',
+        horario: {
+            '9:00': true,
+            '10:00': true,
+            '11:00': true,
+            '12:00': false,
+            '13:00': true,
+            '14:00': true,
+            '15:00': true,
+            '16:00': true,
+            '17:00': true,
+            '18:00': true,
+            '19:00': true,
+            '20:00': true,
+            '21:00': true,
+        }
+    },
+    {
+        dia: 'Quarta',
+        horario: {
+            '9:00': true,
+            '10:00': false,
+            '11:00': true,
+            '12:00': true,
+            '13:00': true,
+            '14:00': true,
+            '15:00': true,
+            '16:00': true,
+            '17:00': true,
+            '18:00': true,
+            '19:00': true,
+            '20:00': true,
+            '21:00': true,
+        }
+    },
+    {
+        dia: 'Quinta',
+        horario: {
+            '9:00': true,
+            '10:00': true,
+            '11:00': true,
+            '12:00': true,
+            '13:00': true,
+            '14:00': false,
+            '15:00': false,
+            '16:00': false,
+            '17:00': true,
+            '18:00': true,
+            '19:00': true,
+            '20:00': true,
+            '21:00': true,
+        }
+    },
+    {
+        dia: 'Sexta',
+        horario: {
+            '9:00': true,
+            '10:00': true,
+            '11:00': true,
+            '12:00': true,
+            '13:00': false,
+            '14:00': true,
+            '15:00': true,
+            '16:00': true,
+            '17:00': true,
+            '18:00': true,
+            '19:00': true,
+            '20:00': true,
+            '21:00': true,
+        }
+    },
+    {
+        dia: 'Sabádo',
+        horario: {
+            '9:00': true,
+            '10:00': true,
+            '11:00': true,
+            '12:00': true,
+            '13:00': true,
+            '14:00': true,
+            '15:00': true,
+            '16:00': false,
+            '17:00': false,
+            '18:00': false,
+            '19:00': true,
+            '20:00': true,
+            '21:00': true,
+        }
+    },
+
+]
